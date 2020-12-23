bca4f15350fce8693f8c51932bd8d79c8d01429d
Fatih KARALAR <45173413+fatihkaralar@users.noreply.github.com>
2020-12-23 21:10:23 +0300

    Expenses list added
    *Users can see the expenses and their details.Also they can see the admin and informations who upload the expense.
    *Some GUI improvements have been made.
    *Fixed a bug where remaining debt was not displayed correctly after the user made a payment.
    *Auto fill disabled for security(In login forms).

--- Project1_DesignApartmentManager/nopermission.php
+++ Project1_DesignApartmentManager/nopermission.php
@@ -1,5 +1,13 @@
-<?php 
-echo "You don't have permission to visit this page";
-
-
- ?>
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>No Permission</title>
+ </head>
+ <body style="background: gray">
+ 	<p style=" text-align: center; font-size: 50px; color: black;">
+ <?php 
+echo "You don't have permission to view this page";
+?>
+</p>
+ </body>
+ </html>
